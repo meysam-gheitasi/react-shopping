47508eb74fb3ddb9b49b12271a4ea74a0ff918cb
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-06-15 01:19:41 +0330

    We find the desired product using id among the products in the products context

--- my-project/src/components/ProductsDetails.js
+++ my-project/src/components/ProductsDetails.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
+// Products Context
+import { ProductsContext } from '../context/ProductsContextProvider';
+
 const ProductsDetails = () => {
+
+    const products = useContext(ProductsContext)
+
     const params = useParams();
+    const product = products[params.id - 1]
+    console.log(product)
 
     return (
         <div>

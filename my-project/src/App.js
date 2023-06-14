25c27d5d5a79daa9c45cac8e1f3cf658512d46b7
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-06-14 23:16:39 +0330

    Creating the Homepage path using the Home component

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,13 +1,16 @@
-
 // Products Context
 import Home from "./components/Home";
 import ProductsContextProvider from "./context/ProductsContextProvider";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="flex justify-center">
       <ProductsContextProvider>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </ProductsContextProvider>
     </div>
   );

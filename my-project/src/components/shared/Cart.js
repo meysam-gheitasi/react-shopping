8276f944d9e0869489c762788ed8d40866d830f1
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-06-14 19:00:57 +0330

    Creating a component cart to display each product separately

---
+++ my-project/src/components/shared/Cart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from "react-router-dom";
+
+const Cart = ( {product} ) => {
+    return (
+        <div className='flex border-solid rounded-lg shadow-md'>
+            <div className='flex flex-col'>
+                <img className='w-52 h-52' src={product.image} alt={product.title} />
+                <h3>{product.title}</h3>
+                <p>{product.price}</p>
+                <div className='flex justify-between'>
+                   <h4 className="text-sky-400">
+                   <Link to="/details">Details</Link>
+                   </h4>
+                   <div>
+                    <button className='btn btn-blue'>Add</button>
+                   </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;

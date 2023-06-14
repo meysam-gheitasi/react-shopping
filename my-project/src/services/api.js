b3bedd996b68dd5352edfde8dd4087e9e597a9d1
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-06-14 17:32:01 +0330

    Create a method to receive products from the API on the first page load of the website and create a context to enable access to products throughout the project.

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 // Define a variable to hold the URL root
-
 const BASE_URL = "https://fakestoreapi.com";
 
 // Create function to get products from API
-
 const getProducts = async () => {
 
     const response = await axios.get(`${BASE_URL}/products`);

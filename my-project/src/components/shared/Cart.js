4efb76f8793ff7d85b117c4c05c2d1a75af0145c
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-06-14 19:58:47 +0330

    Using the shorten method to shorten the title of the products in the Cart component

--- my-project/src/components/shared/Cart.js
+++ my-project/src/components/shared/Cart.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { shorten } from '../../helper/function';
 
 const Cart = ( {product} ) => {
-    console.log(product)
+
     return (
         <div className='flex border-solid rounded-lg shadow-md'>
 
             <div className='flex flex-col'>
 
                 <img className='w-52 h-52' src={product.image} alt={product.title} />
-                <h3>{product.title}</h3>
+                <h3>{shorten(product.title)}</h3>
                 <p>{product.price}</p>
 
                 <div className='flex justify-between'>

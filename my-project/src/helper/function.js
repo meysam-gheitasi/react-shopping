5525b8104e0c2af4f0e8afc481ac3ec386bd050b
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-06-16 01:07:48 +0330

    Implementation of the isInCart method to check the presence of a product already in the shopping cart

--- my-project/src/helper/function.js
+++ my-project/src/helper/function.js
@@ -3,7 +3,12 @@ const shorten = (title) => {
     const splitedTitle = title.split(" ");
     const newTitle = ` ${splitedTitle[0]} ${splitedTitle[1]}`
     return newTitle
-    
+
 }
 
-export { shorten }
+const isInCart = (state, id) => {
+    const result = !!state.selectedItems.find(item => item.id === id)
+    return result
+}
+
+export { shorten, isInCart }

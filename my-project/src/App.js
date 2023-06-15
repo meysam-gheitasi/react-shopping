3ed9d14f526067b0f3236a461467533a01b7a7a0
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-06-15 20:10:24 +0330

    Make the CartContextProvider available in the topmost component for global accessibility in the project

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,6 +1,7 @@
 // Products Context
 import Home from "./components/Home";
 import ProductsDetails from "./components/ProductsDetails";
+import CartContextProvider from "./context/CartContextProvider";
 import ProductsContextProvider from "./context/ProductsContextProvider";
 
 import { Route, Routes } from "react-router-dom";
@@ -9,10 +10,12 @@ function App() {
   return (
     <div className="flex justify-center">
       <ProductsContextProvider>
+        <CartContextProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products/:id" element={<ProductsDetails />} />
         </Routes>
+        </CartContextProvider>
       </ProductsContextProvider>
     </div>
   );

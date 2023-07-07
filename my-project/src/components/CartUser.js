6539c73ec2e8c9068e0e3db0c5aaa1018fb9956e
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-07-07 08:40:36 +0330

    Functionality implementation of DECREASE, REMOVE_ITEM and INCREASE buttons

--- my-project/src/components/CartUser.js
+++ my-project/src/components/CartUser.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-//Context Cart Shopping
+// Context Cart Shopping
 import { CartContext } from '../context/CartContextProvider';
+
+// Functions Helper
+import { shorten } from '../helper/function';
+
+// Icons
+import trash from '../assets/icons/trash.svg'
 
 const CartUser = ({ data }) => {
 
-    
+    const { dispatch } = useContext(CartContext)
 
     const { image, title, price, quantity } = data
 
     return (
         <div>
-            
+            <img src={image} alt='product' />
+            <div>
+                <h3>{shorten(title)}</h3>
+                <p>{price} $</p>
+            </div>
+            <div>
+                <span>{quantity}</span>
+            </div>
+            <div>
+                {
+                    quantity > 1 ?
+                    <button onClick={() => dispatch({ type: "DECREASE", pyload: data })}>-</button> :
+                    <button onClick={() => dispatch({ type: "REMOVE_ITEM", pyload: data})}>{trash}</button>
+                }
+                {
+                    <button onClick={() => dispatch({ type: "INCREASE", pyload: data })}>+</button>
+                }
+            </div>
         </div>
     );
 }

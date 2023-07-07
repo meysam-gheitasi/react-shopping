7576fd36c78a73dab41a51f5fe9a534da7ce9d3b
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-07-07 10:12:05 +0330

    Development of the total shopping cart section including total items and total payments in the ShopCart component

--- my-project/src/components/ShopCart.js
+++ my-project/src/components/ShopCart.js
@@ -10,8 +10,23 @@ const ShopCart = () => {
     const { state, dispatch } = useContext(CartContext)
 
     return (
-        <div className='flex flex-col'>
-            {state.selectedItems.map(item => <CartUser key={item.id} data={item} />)}
+        <div>
+            <div className='flex flex-col'>
+                {state.selectedItems.map(item => <CartUser key={item.id} data={item} />)}
+            </div>
+            <div>
+                {
+                    state.itemCounter > 0 &&
+                    <div>
+                        <p><span> Total Items: </span> {state.itemCounter} </p>
+                        <p><span> Total Payments: </span> {state.total} </p>
+                        <div>
+                            <button onClick={() => {dispatch({ type: 'CHECKOUT' })}}>Check Out</button>
+                            <button onClick={() => {dispatch({ type: 'CLEAR' })}}>Clear</button>
+                        </div>
+                    </div>
+                }
+            </div>
         </div>
     );
 }
